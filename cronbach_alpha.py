37b62cfda559215b19a0d96e8a8a2a75cdee9e60
Roscoe Lai <ahnouuyz@gmail.com>
2020-06-16 15:25:47 +0800

    Made cronbach_alpha.py more readable, possibly less efficient

--- cronbach_alpha.py
+++ cronbach_alpha.py
@@ -1,27 +1,37 @@
-#!/usr/bin/env -S conda run -n env1 python
-# File: cronbach_alpha.py
-
 import numpy as np
 
-def cronbach_alpha(A: np.ndarray) -> float:
-    """Return Cronbach's alpha for the given input array.
+def cronbach_alpha(X: np.ndarray) -> float:
+    """
+    Calculates Cronbach's alpha.
 
-    A : Matrix of numerical values for the relevant columns/features
+    :param X: a matrix of numerical values
+    :returns: Cronbach's alpha
+    :raises TypeError: if X is not all numeric
 
     >>> cronbach_alpha(np.array([[1, 1], [2, 2]]))
     1.0
+
     >>> cronbach_alpha(np.array([[1, 1], [1, 2]]))
     0.0
+
     >>> np.random.seed(123); cronbach_alpha(np.random.randint(0, 5, (30, 5)))
     0.1763577331759146
+
     """
-    A = np.array(A)
-    k = A.shape[1]
-    item_vars = np.nanvar(A, axis=0, ddof=1)
-    sum_items = np.nansum(A, axis=1)
-    ratio = np.sum(item_vars) / np.var(sum_items, ddof=1)
-    return k / (k - 1) * (1 - ratio)
+    A = np.array(X)
+    ncols = A.shape[1]
+
+    variances_of_columns = np.nanvar(A, axis=0, ddof=1)
+    sums_of_rows = np.nansum(A, axis=1)
+
+    sum_of_variances = np.sum(variances_of_columns)
+    variance_of_sums = np.var(sums_of_rows, ddof=1)
+
+    prealpha = 1 - (sum_of_variances / variance_of_sums)
+    alpha = ncols / (ncols - 1) * prealpha
+
+    return alpha
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=True)
