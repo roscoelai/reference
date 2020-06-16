605be971237a06f6d7e2cc04020528a73ea6c2e4
Roscoe Lai <ahnouuyz@gmail.com>
2020-06-16 16:56:10 +0800

    Update documentation for impute_ls.py

--- impute_ls.py
+++ impute_ls.py
@@ -1,70 +1,75 @@
-#!/usr/bin/env -S conda run -n env1 python
-# File: impute_ls.py
-
 import numpy as np
-from typing import Union
 
-def impute_ls(A: np.ndarray, thresh: Union[int, float]=None, rounding=False):
-    """Impute blanks in an array
+def impute_ls(X: np.ndarray, thresh=0.2, rounding=True):
+    """
+    Imputes missing values using the least squares approach.
 
-    Returns the given array with blanks filled in using least-squares 
-    imputation. Only the rows with number of blanks <= thresh would be 
-    imputed. If there are no valid cases for imputation, return the original 
-    array.
+    :param X: a matrix of numerical values, may contain missing values
+    :param thresh: number or proportion of missing values allowed
+    :param rounding: round imputed values to nearest whole number
+    :returns: the original matrix with eligible missing values imputed
 
     >>> print(impute_ls([[1, 1], [1, 1], [1, np.nan]], 1))
     [[1. 1.]
      [1. 1.]
      [1. 1.]]
+
     >>> print(impute_ls([[1, 1], [1, np.nan]], 1))
     [[ 1.  1.]
      [ 1. nan]]
+
     >>> print(impute_ls([[1, 1], [1, np.nan]]))
     [[ 1.  1.]
      [ 1. nan]]
+
     """
-    A = np.array(A)
+    A = np.array(X)
     k = A.shape[1]
 
-    if thresh == None:
-        thresh = k * 0.2
-    elif isinstance(thresh, float):
+    # Check that thresh is valid
+    if isinstance(thresh, int) and (0 <= thresh <= k):
+        pass
+    elif isinstance(thresh, float) and (0. <= thresh <= 1.):
         thresh *= k
+    elif isinstance(thresh, str) and thresh.lower() == "all":
+        thresh = k
+    else:
+        raise ValueError(f"Invalid thresh: {thresh}, {type(thresh)}")
 
-    # Specify rows which may be imputed: 0 < blanks <= thresh
-    eligible = np.isnan(A).any(axis=1) & (np.isnan(A).sum(axis=1) <= thresh)
-    B = A[eligible]
+    # Select rows which can be imputed: 0 < nblanks <= thresh
+    imputable = np.isnan(A).any(axis=1) & (np.isnan(A).sum(axis=1) <= thresh)
+    B = A[imputable]
 
-    # Rows without blanks
+    # Select rows without blanks, to be used as reference
     C = A[np.isfinite(A).all(axis=1)]
 
+    # There must be more reference rows than rows to impute
     if 0 < len(B) < len(C):
-        # Move to 3D to calculate for all rows in B concurrently
-        # Spend some time to understand how the broadcasting works
-        # C.shape (n, k) -> (1, n, k)
-        # B.shape (m, k) -> (m, 1, k), where 1 < m < n
-        # D.shape (m, n, k)
+        """
+        Move to 3 dimensions to vectorize the entire operation
+        Details of broadcasting:
+        C (n x k) -> C[np.newaxis, :] (1 x n x k)
+        B (m x k) -> B[:, np.newaxis] (m x 1 x k), where 0 < m < n
+        D (m x n x k)
+        """
         D = C[np.newaxis, :] - B[:, np.newaxis]
 
         # Calculate sum-of-squares along axis=2 (where the k columns are)
-        sse = np.nansum(np.square(D), axis=2)
+        ss = np.nansum(np.square(D), axis=2)
 
-        # Find the row(s) with the minimum sse
-        # Average rows if there are ties, avoid using np.argmin()
-        E = np.array([C[row == row.min()].mean(axis=0) for row in sse])
+        # Find the row(s) with the minimum ss (average ties)
+        E = np.array([C[rows == rows.min()].mean(axis=0) for rows in ss])
+        if rounding:
+            E = np.round(E)
 
         # Impute for specified rows, then replace the original rows
-        if rounding:
-            F = np.nan_to_num(B) + np.isnan(B) * np.round(E)
-        else:
-            F = np.nan_to_num(B) + np.isnan(B) * E
-        Z = np.copy(A)
-        Z[eligible] = F
+        F = np.nan_to_num(B) + np.isnan(B) * E
 
-    # This criteria is rather arbitrary, need to find some literature here...
+        Z = np.copy(A)
+        Z[imputable] = F
+
     elif len(B) >= len(C):
-        # print("At least half the rows contain blanks!")
-        # print("Perhaps try a different approach?")
+        # print("At least half the rows contain blanks! Process skipped.")
         Z = A
 
     else:
@@ -75,4 +80,4 @@ def impute_ls(A: np.ndarray, thresh: Union[int, float]=None, rounding=False):
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=True)
