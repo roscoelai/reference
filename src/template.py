ac878750694e46fb618825e2e60719f501db12c2
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-02-24 14:41:34 +0800

    Update template.py

--- src/template.py
+++ src/template.py
@@ -3,20 +3,28 @@
 """
 Created on YYYY-MM-DD HH:MM:SS
 
-@author: name
+@author: Name
 """
 
 #%% import-libraries
 import time
 
 #%% define-functions
+def timer(f):
+    def wrapper(*args, **kwargs):
+        t1 = time.perf_counter()
+        result = f(*args, **kwargs)
+        t2 = time.perf_counter()
+        print("Function:", f.__name__)
+        print("Arguments:", args, kwargs)
+        print("Time taken: {:.6f} secs".format(t2 - t1))
+        return result
+    return wrapper
 
 #%% main
+@timer
 def main():
-    t1 = time.perf_counter()
-    
-    t2 = time.perf_counter()
-    print("Time taken: {:.6f} secs".format(t2 - t1))
+    pass
 
 if __name__ == "__main__":
     main()
